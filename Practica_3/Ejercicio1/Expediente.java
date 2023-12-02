0fd898a4927b3a012fff4a0371ee9247d3b6f662
mikzb <mikolajzabski@uma.es>
2023-12-02 17:53:48 +0100

    ej3 vpp brrbrbrbr

--- Practica_3/Ejercicio1/Expediente.java
+++ Practica_3/Ejercicio1/Expediente.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 public class Expediente {
-    private List<Acceso> acceso;
+    private List<Acceso> accesos;
     private Paciente paciente;
 
     public Expediente(Paciente p) {
         assert p != null;
 
-        this.acceso = new ArrayList<Acceso>();
+        this.accesos = new ArrayList<Acceso>();
         this.paciente = p;
 
         assert p.notDuplicatedExpediente();
     }
 
     protected void addAcceso(Acceso a) {
-        this.acceso.add(a);
+        this.accesos.add(a);
     }
 
     protected void removeAcceso(Acceso a) {
-        this.acceso.remove(a);
+        this.accesos.remove(a);
     }
 
     protected Paciente getPaciente() {
         return this.paciente;
     }
 
-    public Enumeration<Acceso> getAcceso() {
-        return Collections.enumeration(acceso);
+    public Enumeration<Acceso> getAccesos() {
+        return Collections.enumeration(accesos);
     }
 }
